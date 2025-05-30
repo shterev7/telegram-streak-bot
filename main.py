f74c752fa6b031d895063e2de8130183e0bfecfc
Stoyan Shterev <stoqn780@gmail.com>
2025-05-30 20:25:39 +0300

    disable logging

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from bot.handlers import handle_all_text
 from bot.reminders import send_daily_reminder
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+
+# logging.basicConfig(
+#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#     level=logging.INFO
+# )
 
 if __name__ == '__main__':
     TOKEN = os.getenv("BOT_TOKEN")
